815ab912b25f71e89cb5d8b643dd9fa84566f30e
daniel cruz mamani <135240392+danielcruzmamani@users.noreply.github.com>
2023-06-09 07:28:12 -0500

    Update 3,3.cpp

--- 3,3.cpp
+++ 3,3.cpp
@@ -1,23 +1,30 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main()
 {
     float a, b, c, d, e, f;
-    cout << "Ingrese los coeficientes a, b, c, d, e y f (en ese orden) de un sistema linear de dos ecuaciones con dos incógnitas:" << endl;
+    cout << "Ingrese los coeficientes a, b, c, d, e y f (en ese orden) de un sistema lineal de dos ecuaciones con dos incógnitas:" << endl;
     cin >> a >> b >> c >> d >> e >> f;
-    
+
     float det = a*d - b*c;
-    
+
     if (det == 0) {
         cout << "El sistema no tiene solución única." << endl;
     } else {
         float x = (d*c - b*f) / det;
         float y = (a*f - e*c) / det;
+
+        if (x > 2 && y > 0) {
+            y = pow(y, 2);
+        } else {
+            y -= 2;
+        }
+
         cout << "La solución del sistema es: x = " << x << ", y = " << y << endl;
     }
-    
+
     return 0;
-} // MODIFICAR EL EJERCICIO PARA EL CASO QUE SI EL RESULTADO DE "X" ES POSITIVO Y MAYOR A 2, EL RESULTADO DE "Y", DEBERA SER ELEVADO AL CUADRADO, 
-// CASO CONTRARIO SE LE RESTARA 2
+}
