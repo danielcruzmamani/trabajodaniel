6ee3e2fe0c41a43e7034ce9bb383d84f4593cb90
daniel cruz mamani <135240392+danielcruzmamani@users.noreply.github.com>
2023-11-23 18:57:03 -0500

    Delete 5.1-junio22.cpp

--- 5.1-junio22.cpp
+++
@@ -1,8 +0,0 @@
-5.1. ¿Cuál es la salida del siguiente segmento de programa?
-for (int cuenta = 1; cuenta < 5; cuenta++)
-cout << (2 * cuenta) << “ “;
-
-//RESOLUCION:
-
-La salida total del segmento de
-programa es: 2 4 6 8.
