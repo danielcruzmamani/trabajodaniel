2f485ed0f0720ad52f5449c7e4c1944353acfa07
daniel cruz mamani <135240392+danielcruzmamani@users.noreply.github.com>
2023-11-25 12:58:03 -0500

    Create footer.php

---
+++ Views/Templates/footer.php
@@ -0,0 +1,31 @@
+</div>
+</main>
+<footer class="py-4 bg-light mt-auto">
+    <div class="container-fluid px-4">
+        <div class="d-flex align-items-center justify-content-between small">
+            <div class="text-muted">Copyright &copy; Your Website 2021</div>
+            <div>
+                <a href="#">Privacy Policy</a>
+                &middot;
+                <a href="#">Terms &amp; Conditions</a>
+            </div>
+        </div>
+    </div>
+</footer>
+</div>
+</div>
+
+<script src="<?php echo base_url; ?>Assets/js/jquery-3.6.0.min.js"></script>
+<script src="<?php echo base_url; ?>Assets/js/jquery-ui.min.js"></script>
+<script src="<?php echo base_url; ?>Assets/DataTables/datatables.min.js"></script>
+<script src="<?php echo base_url; ?>Assets/js/ckeditor.js"></script>
+<script src="<?php echo base_url; ?>Assets/js/scripts.js"></script>
+<script>
+    const base_url = "<?php echo base_url; ?>";
+</script>
+<script src="<?php echo base_url; ?>Assets/js/sweetalert2.all.min.js"></script>
+<script src="<?php echo base_url; ?>Assets/js/chart.min.js"></script>
+<script src="<?php echo base_url; ?>Assets/js/funciones.js"></script>
+</body>
+
+</html>
