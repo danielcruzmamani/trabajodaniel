ec0e9ae40e379e702a7d7d4ec533e1eb8e8fc8c4
daniel cruz mamani <135240392+danielcruzmamani@users.noreply.github.com>
2023-11-25 12:56:20 -0500

    Create index.php

---
+++ Views/Errores/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Pagina no encontrada</title>
+    <link rel="stylesheet" href="<?php echo base_url; ?>Assets/css/estilos.css">
+</head>
+
+<body>
+
+    <div class="content">
+        <a href="<?php echo base_url; ?>Administracion/home" class="pagina">Vida Informático</a>
+        <hr>
+        <span class="tags">Página no Encontrada</span>
+        <p>La página que buscas no existe, ponte en contacto con el administrador.</p>
+    </div>
+
+</body>
+
+</html>
